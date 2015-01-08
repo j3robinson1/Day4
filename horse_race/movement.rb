79bfeaa70d31a74109df79904ea7249c2499a78e
Jesse Robinson <j3robinson1@gmail.com>
2015-01-08 14:11:44 -0500

    fixed minor things

--- horse_race/movement.rb
+++ horse_race/movement.rb
@@ -9,26 +9,26 @@ class Location
 		puts "press Enter to move on"
 		continue = gets.chomp
 		self.distance_traveled = rand(1..5)
-		if distance_traveled = 1
+		if distance_traveled == 1
 			puts "~"
 			@total += distance_traveled
-		elsif distance_traveled = 2
+		elsif distance_traveled == 2
 			puts "~~"
 			@total += distance_traveled
-		elsif distance_traveled = 3
+		elsif distance_traveled == 3
 			puts "~~~"
 			@total += distance_traveled
-		elsif distance_traveled = 4
+		elsif distance_traveled == 4
 			puts "~~~~"
 			@total += distance_traveled
-		elsif distance_traveled = 5
+		elsif distance_traveled == 5
 			puts "~~~~~"
 			@total += distance_traveled
 		end
-			line1 = "#{@total}#{horse1}"
-			line2 = "#{@total}#{horse2}"
-			line3 = "#{@total}#{horse3}"
-			line4 = "#{@total}#{horse4}"
+			line1 == "#{@total}#{horse1}"
+			line2 == "#{@total}#{horse2}"
+			line3 == "#{@total}#{horse3}"
+			line4 == "#{@total}#{horse4}"
 	end
 	def cheat
 		if continue.downcase = "cheat"
